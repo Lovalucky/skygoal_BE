e1d428a6297c45a72015b1eaea2600158937bd45
Lovalucky <lovaprasad230@gmail.com>
2024-10-20 13:28:26 +0530

    prasad_be

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/user-controller')
 
 router.post("/signup",userController.signup); 
 router.post("/login", userController.login); 
-router.get("/",userController.protect,userController.getAllUsers)
+router.get("/",userController.getAllUsers)
 
 router.route('/forgotPassword').post(userController.forgotPassword);
 router.route('/resetPassword/:token').patch(userController.resetPassword);
